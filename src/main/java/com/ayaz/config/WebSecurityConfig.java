70e2936c6cc881df12a7cebc760c1453318b1ba2
Ayaz <53002825+ayaznacafli@users.noreply.github.com>
2020-01-28 16:05:01 +0300

    Spring Security On Spring Boot

---
+++ src/main/java/com/ayaz/config/WebSecurityConfig.java
@@ -0,0 +1,57 @@
+package com.ayaz.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService customUserDetailsService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+        auth.userDetailsService(this.customUserDetailsService).passwordEncoder(this.passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/resources/","/webjars/","/assets/")
+                   .permitAll().antMatchers("/")
+                   .permitAll().antMatchers("/admin/").hasRole("ADMIN")
+                   .antMatchers().authenticated()
+                   .and()
+
+                .formLogin()
+                   .loginPage("/login")
+                   .defaultSuccessUrl("/home")
+                   .failureUrl("/login?error")
+                   .permitAll()
+                   .and()
+
+                .logout()
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                    .logoutSuccessUrl("/login?logout")
+                    .deleteCookies("remember-me")
+                    .permitAll()
+                    .and()
+                .rememberMe()
+                     .and()
+                .exceptionHandling()
+                     .accessDeniedPage("/accessDenied");
+    }
+}
